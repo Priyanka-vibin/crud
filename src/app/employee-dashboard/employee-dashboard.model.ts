d34ae16285313f2be1460f3cc69c431a25e3e62f
Priyank-vibin <vibinpriya91@gmail.com>
2022-11-30 10:49:12 +0530

    crud-opertion

---
+++ src/app/employee-dashboard/employee-dashboard.model.ts
@@ -0,0 +1,8 @@
+export class EmployeeModel{
+    id: number = 0;
+    firstName : string= '';
+    lastname : string= '';
+    email : string= '';
+    mobile : string= '';
+    salary : string= '';
+}
